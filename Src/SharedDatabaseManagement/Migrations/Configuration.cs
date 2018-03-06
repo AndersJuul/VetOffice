01d2806008c4790f6132f9e05da77a7748fe59b4
AndersJuul <andersjuulsfirma@gmail.com>
2018-03-06 13:40:56 +0100

    initial

---
+++ Src/SharedDatabaseManagement/Migrations/Configuration.cs
@@ -0,0 +1,33 @@
+namespace VetOffice.SharedDatabase.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+    using VetOffice.SharedDatabase.DataModel;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<VetOffice.SharedDatabase.DataModel.VetOfficeContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(VetOffice.SharedDatabase.DataModel.VetOfficeContext context)
+        {
+            new TestInitializerForVetContext().Seed1(context);
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
